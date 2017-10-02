98ace442d1f1afd414be9121f48dfb7fefe36eca
liamclose <liamclose@cb5109-36.labs.sce.carleton.ca>
2017-10-02 11:49:15 -0400

    Made Change

--- src/Hello.java
+++ src/Hello.java
@@ -1,4 +1,18 @@
 
 public class Hello {
+	
+	public static void main(String[] args) 
+	{
+		Hello hello = new Hello();
+		hello.methodA();
+		hello.methodB();
+		hello.methodC();
+	}
+
+	public void methodA( ) { }
+
+	public void methodB() { }
+
+	public void methodC() { }
 
 }
